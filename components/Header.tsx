2c809701f5e7a4884fdbbf182e7af1c1210d657f
Jordi Castro <jordi.castro003@gmail.com>
2024-07-15 17:46:50 -0500

    auth modal + toaster provider

--- components/Header.tsx
+++ components/Header.tsx
@@ -7,6 +7,11 @@ import { RxCaretRight as RightArrow } from "react-icons/rx";
 import { HiHome as HomeIcon } from "react-icons/hi";
 import { BiSearch as SearchIcon } from "react-icons/bi";
 import Button from "@/components/Button";
+import useAuthModal from "@/hooks/useAuthModal";
+import { useSupabaseClient } from "@supabase/auth-helpers-react";
+import { useUser } from "@/hooks/useUser";
+import { FaUserAlt } from "react-icons/fa";
+import toast from "react-hot-toast";
 
 interface HeaderProps {
     children: React.ReactNode;
@@ -15,9 +20,22 @@ interface HeaderProps {
 
 const Header = ({children, className}: HeaderProps) => {
     const router = useRouter();
+    const authModal = useAuthModal();
 
-    const handleLogout = () => {
-        // handle logout
+    const supabaseClient = useSupabaseClient();
+    const { user } = useUser();
+
+    const handleLogout = async () => {
+        const { error } = await supabaseClient.auth.signOut();
+
+        // TODO: reset any playing songs
+        router.refresh();
+
+        if (error) {
+            toast.error(error.message);
+        } else {
+            toast.success("You have been logged out");
+        }
     }
 
     return (
@@ -67,11 +85,29 @@ const Header = ({children, className}: HeaderProps) => {
             <div
                 className="flex justify-between items-center gap-x-4"
             >
+                {user ? (
+                    <div
+                        className="flex gap-x-4 items-center"
+                    >
+                        <Button
+                            onClick={handleLogout}
+                            className="bg-white px-6 py-2"
+                        >
+                            Logout
+                        </Button>
+                        <Button
+                            onClick={() => router.push("/account")}
+                            className="bg-white"
+                        >
+                            <FaUserAlt />
+                        </Button>
+                    </div>
+                ):(
                 <>
                     <div>
                         <Button
                             className="bg-transparent text-neutral-300 font-medium"
-                            onClick={() => {}}
+                            onClick={authModal.onOpen}
                         >
                             Sign up
                         </Button>
@@ -80,12 +116,13 @@ const Header = ({children, className}: HeaderProps) => {
                     <div>
                         <Button
                             className="bg-white px-6 py-2"
-                            onClick={() => {}}
+                            onClick={authModal.onOpen}
                         >
                             Log in
                         </Button>
                     </div>
                 </>
+                )}
             </div>
         </div>
         {children}
